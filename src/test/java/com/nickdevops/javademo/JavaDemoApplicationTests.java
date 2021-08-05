fcdabbadff03b431dc44aae13506cab15d13b3ae
Nick Wen <1006953115@qq.com>
2021-08-05 18:25:22 +0800

    first commit

---
+++ src/test/java/com/nickdevops/javademo/JavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickdevops.javademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
